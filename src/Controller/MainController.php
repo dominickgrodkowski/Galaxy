0f3574aff630f6157d678a22cf668fd81a31915f
Dominick Grodkowski <Dominick.Grodkowski@gmx.de>
2024-09-11 11:19:28 +0200

    saving work

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -7,25 +7,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-
 class MainController extends AbstractController
 {
-    #[Route('/')]
+    #[Route('/', name: 'app_homepage')]
     public function homepage(StarshipRepository $starshipRepository): Response
     {
         $ships = $starshipRepository->findAll();
-
-
         $myShip = $ships[array_rand($ships)];
 
-
-
-
-
-
         return $this->render('main/homepage.html.twig', [
-            'ships' => $ships,
             'myShip' => $myShip,
+            'ships' => $ships,
         ]);
     }
 }
